95cb85c09ed400c1ee931e41be23d5e515ca4792
Youssef Moataz <yousefcrunshy03@gmail.com>
2024-05-09 13:33:30 +0300

    Update available places.

--- app/othello.py
+++ app/othello.py
@@ -2,12 +2,15 @@ from utils import *
 
 class Othello:
     def __init__(self):
+        self.di = [0, 1, 0, -1]
+        self.dj = [1, 0, -1, 0]
+
         self.board = [[EMPTY for _ in range(8)] for _ in range(8)]
         self.board[3][3] = self.board[4][4] = WHITE_DISK
         self.board[3][4] = self.board[4][3] = BLACK_DISK
-        self.calculate_available_for(BLACK_DISK)
 
         self.current_player = BLACK_DISK
+        self.calculate_available_for(BLACK_DISK)
 
     def print_board(self):
         for i in range(8):
@@ -19,16 +22,14 @@ class Othello:
         return 0 <= i < 8 and 0 <= j < 8
 
     def set_available_around(self, i, j):
-        di = [0, 1, 0, -1]
-        dj = [1, 0, -1, 0]
-
         for c in range(4):
-            currenti = i + di[c]
-            currentj = j + dj[c]
+            currenti = i + self.di[c]
+            currentj = j + self.dj[c]
             if not self.is_in_board(currenti, currentj):
                 continue
             if self.board[currenti][currentj] == EMPTY:
-                self.board[currenti][currentj] = AVAILABLE
+                if(self.can_outflank(currenti, currentj, -self.di[c], -self.dj[c], self.current_player)):
+                    self.board[currenti][currentj] = AVAILABLE
 
     def get_board_1d(self):
         board_1d = []
@@ -60,47 +61,22 @@ class Othello:
                     self.board[i][j] = EMPTY
         
     def outflank(self, i, j, player):
-        self.outflank_right(i, j, player)
-        self.outflank_left(i, j, player)
-        self.outflank_up(i, j, player)
-        self.outflank_down(i, j, player)
+        for c in range(4):
+            self.outflank_direction(i, j, self.di[c], self.dj[c], player)
         self.clear_available()
-            
-    def outflank_right(self, i, j, player):
-        last_i, last_j = i, j
-
-        opponent = self.get_opponent(player)
-
-        flag = False
-        for c in range(1, 8):
-            last_j = j + c
-            if not self.is_in_board(last_i, last_j):
-                break
-
-            if self.board[last_i][last_j] == opponent:
-                continue
-            
-            if self.board[last_i][last_j] == player:
-                flag = True
-                break
-
-        # print(i, j, last_i, last_j)
-
-        if flag:
-            for c in range(j, last_j):
-                self.board[last_i][c] = player
                 
-    def outflank_left(self, i, j, player):
+    def outflank_direction(self, i, j, di, dj, player):
         last_i, last_j = i, j
-
         opponent = self.get_opponent(player)
-
         flag = False
-        for c in range(1, 8):
-            last_j = j - c
+        
+        for _ in range(1, 8):
+            last_i += di
+            last_j += dj
+            
             if not self.is_in_board(last_i, last_j):
                 break
-
+            
             if self.board[last_i][last_j] == opponent:
                 continue
             
@@ -108,23 +84,24 @@ class Othello:
                 flag = True
                 break
 
-        # print(i, j, last_i, last_j)
-
         if flag:
-            for c in range(last_j, j):
-                self.board[last_i][c] = player
+            while (i, j) != (last_i, last_j):
+                self.board[i][j] = player
+                i += di
+                j += dj
     
-    def outflank_up(self, i, j, player):
+    def can_outflank(self, i, j, di, dj, player):
         last_i, last_j = i, j
-
         opponent = self.get_opponent(player)
-
         flag = False
-        for c in range(1, 8):
-            last_i = i - c
+        
+        for _ in range(1, 8):
+            last_i += di
+            last_j += dj
+            
             if not self.is_in_board(last_i, last_j):
                 break
-
+            
             if self.board[last_i][last_j] == opponent:
                 continue
             
@@ -132,35 +109,7 @@ class Othello:
                 flag = True
                 break
 
-        # print(i, j, last_i, last_j)
-
-        if flag:
-            for c in range(last_i, i):
-                self.board[c][last_j] = player
-    
-    def outflank_down(self, i, j, player):
-        last_i, last_j = i, j
-
-        opponent = self.get_opponent(player)
-
-        flag = False
-        for c in range(1, 8):
-            last_i = i + c
-            if not self.is_in_board(last_i, last_j):
-                break
-
-            if self.board[last_i][last_j] == opponent:
-                continue
-            
-            if self.board[last_i][last_j] == player:
-                flag = True
-                break
-
-        # print(i, j, last_i, last_j)
-
-        if flag:
-            for c in range(i, last_i):
-                self.board[c][last_j] = player
+        return flag
 
     def player_clicked(self, i, j):
         if self.board[i][j] == AVAILABLE:

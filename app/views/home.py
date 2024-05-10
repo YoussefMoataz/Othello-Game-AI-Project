db68307dee2ae7360004021296ee1215674db90a
Youssef Moataz <yousefcrunshy03@gmail.com>
2024-05-10 10:57:00 +0300

    Update home buttons UI.

--- app/views/home.py
+++ app/views/home.py
@@ -17,6 +17,8 @@ def Home(page: ft.Page, params, basket):
         set_othello(Othello(DIFF_HARD))
         page.go("/game")
 
+    button_height, button_width = 60, 140
+
     return ft.View("/", 
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         vertical_alignment=ft.MainAxisAlignment.CENTER,
@@ -24,8 +26,8 @@ def Home(page: ft.Page, params, basket):
             spacing=40,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             controls=[
-                ft.FilledButton("Easy", on_click=easy_clicked), 
-                ft.FilledButton("Medium", on_click=medium_clicked), 
-                ft.FilledButton("Hard", on_click=hard_clicked)
+                ft.FilledButton("Easy", on_click=easy_clicked, height=button_height, width=button_width), 
+                ft.FilledButton("Medium", on_click=medium_clicked, height=button_height, width=button_width), 
+                ft.FilledButton("Hard", on_click=hard_clicked, height=button_height, width=button_width)
             ])
         ])
